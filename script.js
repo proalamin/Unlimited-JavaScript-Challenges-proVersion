d690fd3642a7ba44ef63aabeffc5d3624f644771
proalamin <pro.alamin247@gmail.com>
2023-10-09 19:16:54 +0600

    add api->input city name than fetch data and than show this data weather details area

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('js connected')
+console.log('js connected');
+
+const apiKey="b0fc41b2b17df2c39088a880df0d009a";
+const apiUrl= "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox= document.querySelector(".search input");
+const searchBtn= document.querySelector(".search button");
+
+async function checkWeather(city){
+    const response =await fetch(apiUrl +city+ `&appid=${apiKey}`);
+    var data= await response.json();
+    
+    console.log(data)
+    document.querySelector('.city').innerHTML= data.name;
+    document.querySelector('.temp').innerHTML= Math.round(data.main.temp);
+    document.querySelector('.humidity').innerHTML= data.main.humidity;
+    document.querySelector('.wind').innerHTML= data.wind.speed;
+}
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
+
