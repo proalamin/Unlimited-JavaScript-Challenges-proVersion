a82b3d01cf6868391aa8e0e82d272bafccddc232
proalamin <pro.alamin247@gmail.com>
2023-10-13 22:00:40 +0600

    modal box-> submit btn and close btn funtionality added, all are good

--- modal-box/script.js
+++ modal-box/script.js
@@ -1,1 +1,11 @@
 console.log('js connected')
+
+let popup = document.getElementById("popup");
+
+function submitBtn(){
+    popup.classList.add('open-popup')
+}
+
+function closeBtn(){
+    popup.classList.remove('open-popup')
+}
