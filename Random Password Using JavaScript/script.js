14d35e383aaadca507673e03a02859400d630b52
proalamin <pro.alamin247@gmail.com>
2023-10-14 00:31:40 +0600

    random password js -> generate password button functionality added and successfully show the random password in the password filed

--- Random Password Using JavaScript/script.js
+++ Random Password Using JavaScript/script.js
@@ -1,1 +1,24 @@
-console.log('js connected')
+console.log("js connected");
+
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "!@#$%^&*()_+-=[]{}|:;<>,.?/";
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += upperCase[Math.floor(Math.random() * lowerCase.length)];
+  password += upperCase[Math.floor(Math.random() * number.length)];
+  password += upperCase[Math.floor(Math.random() * symbol.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+  passwordBox.value=password;
+}
