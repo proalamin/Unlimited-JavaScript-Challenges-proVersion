6360eda2016a01469102e470cd2ec7a8d3b91706
proalamin <pro.alamin247@gmail.com>
2023-10-15 02:23:44 +0600

    facebook friend request-> Add Friends btn first click friends make done and add btn change to remove than again click remove friends

--- Facebook Friend Request/script.js
+++ Facebook Friend Request/script.js
@@ -1,15 +1,31 @@
-console.log('ok');
+console.log("ok");
 
-var statusInfo= document.getElementById('statusInfo');
-var addBtn=document.getElementById('addBtn');
-var removeBtn=document.getElementById('removeBtn');
+var statusInfo = document.getElementById("statusInfo");
+var addBtn = document.getElementById("addBtn");
+// var removeBtn=document.getElementById('removeBtn');
 
-addBtn.addEventListener("click", function(){
-    statusInfo.innerHTML='Friend'
-    statusInfo.style.color='green'
-})
+var checkValue = 0;
 
-removeBtn.addEventListener("click", function(){
-    statusInfo.innerHTML='Stranger'
-    statusInfo.style.color='red'
-})
+addBtn.addEventListener("click", function () {
+  if (checkValue == 0) {
+    statusInfo.innerHTML = "Friend";
+    statusInfo.style.color = "green";
+    addBtn.innerHTML='Remove';
+    addBtn.style.background='#dadada';
+    addBtn.style.color='#111';
+    checkValue = 1;
+  }
+  else {
+    statusInfo.innerHTML = "Stranger";
+    statusInfo.style.color = "red";
+    addBtn.innerHTML='Add Friend';
+    addBtn.style.background='#358144e0';
+    addBtn.style.color='#fff';
+    checkValue = 0;
+  }
+});
+
+// removeBtn.addEventListener("click", function(){
+//     statusInfo.innerHTML='Stranger'
+//     statusInfo.style.color='red'
+// })
