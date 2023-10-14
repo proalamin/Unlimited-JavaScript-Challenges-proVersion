82ccdcf29ae823a9eed5c676ba76e84164f0ac38
proalamin <pro.alamin247@gmail.com>
2023-10-15 01:22:37 +0600

    facebook friend request-> project initialization

--- Random Password Using JavaScript/script.js
+++ Random Password Using JavaScript/script.js
@@ -1,7 +1,7 @@
 console.log("js connected");
 
 const passwordBox = document.getElementById("password");
-const length = 12;
+const length = 8;
 
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
