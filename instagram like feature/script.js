d56273a19b9fbad0243c2c973d8cd6c7ac4df762
proalamin <pro.alamin247@gmail.com>
2023-10-29 19:12:05 +0600

    add setTimeout -after 2 seconds of double clicked, the love icon will be hidden

--- instagram like feature/script.js
+++ instagram like feature/script.js
@@ -5,4 +5,13 @@ var love= document.querySelector('i');
 
 con.addEventListener('dblclick', function(){
     love.style.transform= "translate(-50%, -50%) scale(1)";
+    love.style.opacity= 0.85;
+    
+
+    setTimeout(function(){
+        love.style.opacity= 0;
+    }, 1000)
+    setTimeout(function(){
+        love.style.transform= "translate(-50%, -50%) scale(0)";
+    }, 2000)
 })
